257e6fd9adb32382a0d37a0598d0912d06c194b7
geofell <geofell22@gmail.com>
2019-09-04 16:14:09 -0400

    added 3 routes, userprofile, savetripplans, gettripplans

--- db/orm-models.js
+++ db/orm-models.js
@@ -1,71 +1,25 @@
 var orm = require("../config/orm.js");
 
-var bucklistQueries = {
-  selectAll: function(cb) {
-    orm.selectAll("*", "categories", function(res) {
-      cb(res);
-    });
-  },
-  selectAllActivities: function(cb) {
-    orm.selectAll("*", "activities", function(res) {
-      cb(res);
-    });
-  },
+var ormQueries = {
   selectUserPofile: function(findUser, cb) {
     // tableInput, colToSearch, valOfCol, cb
     orm.selectUserPofile("userProfiles", "uid", findUser, function(res) {
       cb(res);
     });
   },
-  selectActivitiesByCategory: function(findCategory, cb) {
+
+  insertData: function(cols, vals, cb) {
+    orm.insertData("tripPlans", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+
+  getTripPlans: function(findUser, cb) {
     // tableInput, colToSearch, valOfCol, cb
-    orm.selectActivitiesByCategory(
-      "activities",
-      "categoryId",
-      findCategory,
-      function(res) {
-        cb(res);
-      }
-    );
-  },
-  selectMyList: function(findUser, cb) {
-    // tableInput, colToSearch, valOfCol, cb
-    orm.selectMyList(findUser, function(res) {
-      cb(res);
-    });
-  },
-  selectCategories: function(cb) {
-    // whatToSelect, tableInput
-    orm.selectCategories("*", "categories", function(res) {
-      cb(res);
-    });
-  },
-  findBuddies: function(actId, uid, cb) {
-    // valOfActId, valOfUid
-    orm.findBuddies(actId, uid, function(res) {
-      cb(res);
-    });
-  },
-  insertMyList: function(cols, vals, cb) {
-    orm.insertMyList("bucketLists", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  insertUser: function(cols, vals, cb) {
-    orm.insertUser("userProfiles", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  deleteMyList: function(condition, cb) {
-    orm.deleteMyList("bucketLists", condition, function(res) {
-      cb(res);
-    });
-  },
-  countActivities: function(cb) {
-    orm.countActivities(function(res) {
+    orm.getTripPlans(findUser, function(res) {
       cb(res);
     });
   }
 };
 
-module.exports = bucklistQueries;
+module.exports = ormQueries;
