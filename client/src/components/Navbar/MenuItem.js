83645b5aaec26c0914cfaaa369488770733e1173
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-09-04 14:34:02 -0400

    creating planner page

---
+++ client/src/components/Navbar/MenuItem.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+
+  function MenuItem(props) {
+    return (
+      <div 
+        className='menu-item'
+        id={ props.item.text }
+        onClick={ props.handleClick(props.item.text) }
+      >
+        { props.item.text.toUpperCase() }
+      </div>
+    )
+  }
+
+  export default MennuItem;
