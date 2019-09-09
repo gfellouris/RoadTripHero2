b21dd6871c0b3921d5d0ee7017ae37337e901ec4
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-09-09 16:56:06 -0400

    planner and delte button de bug

--- client/src/components/Buttons/Buttons.js
+++ client/src/components/Buttons/Buttons.js
@@ -5,7 +5,7 @@ export default class Buttons extends React.Component {
   render() {
     return (
       <>
-        <Button color={this.props.btnStyle} size="lg" onClick={this.props.onClickEvent} disabled={this.props.isInvalid}>{this.props.btnName} </Button>
+        <Button color={this.props.btnStyle} size="md" onClick={this.props.onClickEvent} disabled={this.props.isInvalid}>{this.props.btnName} </Button>
       </>
     );
   }
