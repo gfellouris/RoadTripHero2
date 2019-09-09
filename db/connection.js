ff81063617c0ae38ce616117f9a8215c4689afa1
Greg Manco <gregmanco@gmail.com>
2019-09-09 19:38:36 -0400

    Changed pw back to default

--- db/connection.js
+++ db/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "bouncer88",
+        password: "password",
         database: "roadtriphero_db"
     });
 }
