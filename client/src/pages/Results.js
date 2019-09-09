574240d76473357f55c382aaeb2fc072d7c3af84
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-09-09 19:35:18 -0400

    making results page semi functional

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -1,12 +1,44 @@
-import React from "react";
-import LeafletMap from "../components/Directions/LeafletMap";
+import React, { Component } from "react";
+import TripList from "../components/TripList/TripList.js";
+import { Col, Row, Container } from "../components/Grid/index.js";
+import Header from '../components/Header/Header.js'
+import GlobalContext from '../context/'
+import Navbar from '../components/Navbar/index.js'
+import MapLeaf from '../components/Map/MapLeaf'
 
-function App() {
-  return (
-    <div>
-      <LeafletMap />
-    </div>
-  );
-}
 
-export default App;
+import "../pages/planner.css"
+
+class Results extends Component {
+    static contextType = GlobalContext
+
+    render() {
+        // console.log(this.GlobalContext.user.uid)
+        return (
+            <>
+           <Header/>
+                <Navbar />
+                <Container fluid>
+                  <Row>
+                  <Col size="md-1" >
+                    
+                    
+                    </Col>
+                    <Col size="md-5" >
+                    <MapLeaf/>
+                    
+                    </Col>
+                    <Col size="md-5" >
+                    
+                   <TripList/>
+                    </Col>
+                  </Row>
+                    
+                    
+                </Container>
+            </>
+        )
+    }
+};
+
+export default Results;
